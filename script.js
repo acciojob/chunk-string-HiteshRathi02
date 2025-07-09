b343f5f6e3b614dbf1e46c07f0b031cfa911712f
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-07-09 11:34:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function stringChop(str, size) {
   // your code here
+	const chunk =[];
+  let i=0;
+  while(i< str.length){
+    chunk.push(str.substring(i, i+size));
+    i += size;
+  }
+  return chunk;
 }
 
 // Do not change the code below
